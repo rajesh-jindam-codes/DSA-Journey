1315560ab99800e8fe7ea7d5d493c5f45a885f91
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-11 13:16:08 +0530

    inserting values at certaiin position

---
+++ SinglyLinkedList/insertingNodesatPosition.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+class singlyLinkedList:
+    def __init__(self):
+        self.head=None
+    def append(self,val):
+        newNode=Node(val)
+        if self.head is None:
+            self.head=newNode
+        else:
+            curr=self.head
+            while curr.next is not None:
+                curr=curr.next
+            curr.next=newNode
+    def insertatPosition(self,val,position):
+        newNode=Node(val)
+        if self.head==None or position==0:
+            newNode.next=self.head
+            self.head=newNode
+        else:
+            curr=self.head
+            prev=None
+            count=0
+            while curr.next is not None and count<position-1:
+                prev=curr
+                curr=curr.next
+                count+=1
+            newNode.next=curr.next
+            curr.next=newNode
+    def printList(self):
+        temp=self.head
+        while temp:
+            print(temp.val,end="->")
+            temp=temp.next
+        print('None')
+sll=singlyLinkedList()
+sll.append(10)
+sll.append(20)
+sll.insertatPosition(30,2)
+sll.printList()
