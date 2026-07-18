c4a128e688cc2680d0a6895700e01c5e235cc62e
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-18 23:28:34 +0530

    subset sum using recrsion

---
+++ DoublyLinkedList/Subset Sums.py
@@ -0,0 +1,12 @@
+nums=[1,2,2]
+result=[]
+def solve(index,nums,subset,result):
+    if index>=len(nums):
+        result.append(sum(subset))
+        return
+    subset.append(nums[index])
+    solve(index+1,nums,subset,result)
+    subset.pop()
+    solve(index+1,nums,subset,result)
+solve(0,nums,[],result)
+print(result)
