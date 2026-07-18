b00f9b67028b3c75be5e33a2b63983e6aad1ce06
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-18 23:01:51 +0530

    Binary Numbers generating using recursion

--- Recursion/BinarySubstrings.py
+++ Recursion/BinarySubstrings.py
@@ -1,19 +1,18 @@
-def solve(index,flag,numbers,result,count):
+def solve(index,flag,numbers,result):
     if index==len(numbers):
-        result.append("".join(numbers))
-        count[0]+=1
+        result.append(''.join(numbers))
         return
-    numbers[index]="0"
-    solve(index+1,True,numbers,result,count)
+    
+    numbers[index]='0'
+    solve(index+1,True,numbers,result)
     if flag==True:
         numbers[index]='1'
-        solve(index+1,False,numbers,result,count)
+        solve(index+1,False,numbers,result)
         numbers[index]='0'
-def BinaryNumbers(n):
-    numbers=['0']*n
+
+def binaryNumbers(n):
+    numbers=['0']*(n)
     result=[]
-    count=[0]
-    solve(0,True,numbers,result,count)
-    return result,count[0]
-n=int(input("Enter: "))
-print(BinaryNumbers(n))
+    solve(0,True,numbers,result)
+    return result
+print(binaryNumbers(3))
