79478b36fe57ede68c2b6d8ace06a57ed81d973e
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-22 08:27:35 +0530

    fruits into basket

--- Arrays/FruitsIntoBasket.py
+++ Arrays/FruitsIntoBasket.py
@@ -1,15 +1,19 @@
 def totalFruits(fruits):
     maxLen=0
     n=len(fruits)
-    for i in range(n):
-        myset=set()
-        for j in range(i,n):
-            myset.add(fruits[j])
-            if len(myset)>2:
-                break
-            maxLen=max(maxLen,j-i+1)
+    mydict={}
+    left=0
+    right=0
+    while right<n:
+        mydict[fruits[right]]=mydict.get(fruits[right],0)+1
+        if len(mydict)>2:
+            mydict[fruits[left]]-=1
+            if mydict[fruits[left]]==0:
+                del mydict[fruits[left]]
+            left+=1
+        if len(mydict)<=2:
+            maxLen=max(maxLen,right-left+1)
+        right+=1
     return maxLen
-
 fruits=[3,3,3,1,2,1,1,2,3,4]
-
-print(totalFruits(fruits))    
+print(totalFruits(fruits))
