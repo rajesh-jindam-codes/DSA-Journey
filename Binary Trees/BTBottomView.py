2094a3db1729dd8bc99961e5969dbeb08244fa4f
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-24 12:06:05 +0530

    bianry tree bottom view

---
+++ Binary Trees/BTBottomView.py
@@ -0,0 +1,41 @@
+from collections import deque
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+root = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+node6 = Node(6)
+node7 = Node(7)
+
+root.left = node2
+root.right = node3
+
+node2.left = node4
+node2.right = node5
+
+node3.left = node6
+node3.right = node7
+def bottomView(node):
+    if node is None:
+        return 
+    queue=deque()
+    result={}
+    ans=[]
+    queue.append((node,0))
+    while queue:
+        e,line=queue.popleft()
+        result[line]=e.val
+        if e.left:
+            queue.append((e.left,line-1))
+        if e.right:
+            queue.append((e.right,line+1))
+    for value in sorted(result.items()):
+        ans.append(value[1])
+    return ans
+print(bottomView(root))
