ffbf4277205076175b8559216578be962e079f17
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-26 12:39:24 +0530

    maximum product of two numbers

---
+++ Arrays/MaximumProduct2Nums.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self,n):
+        first=0
+        second=0
+        while n>0:
+            digit=n%10
+            if digit>=first:
+                second=first
+                first=digit
+            elif digit>second:
+                second=digit
+            n//=10
+        return first*second
+obj=Solution()
+print(obj.maxProduct(56))
