f74e4af195b0511aef9d78a105bd49ef5007d8ca
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-27 09:59:04 +0530

    Number of Enclaves using BFS leetcode 1020

--- Graphs/NumberOFEnclaves(leetcode 1020).py
+++ Graphs/NumberOFEnclaves(leetcode 1020).py
@@ -1,22 +1,22 @@
 from collections import deque
 class Solution:
-    def numofEnclaves(self,grid):
+    def numberOfEnclaves(self,grid):
         queue=deque()
+        count=0
         rows=len(grid)
         cols=len(grid[0])
-        count=0
         for r in range(rows):
             for c in range(cols):
                 if (r==0 or c==0 or r>=rows-1 or c>=cols-1) and grid[r][c]==1:
-                    queue.append((r,c))
                     grid[r][c]='S'
+                    queue.append((r,c))
         while len(queue)!=0:
             i,j=queue.popleft()
-            for dx,dy in [(-1,0),(1,0),(0,1),(0,-1)]:
-                newI,newJ=i+dx,j+dy
+            for x,y in [(-1,0),(1,0),(0,1),(0,-1)]:
+                newI,newJ=i+x,j+y
                 if newI<0 or newJ<0 or newI>=rows or newJ>=cols:
                     continue
-                if grid[newI][newJ]!=1:
+                if grid[newI][newJ]!=-1:
                     continue
                 grid[newI][newJ]='S'
                 queue.append((newI,newJ))
@@ -29,4 +29,4 @@ class Solution:
         return count
 grid=[[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
 obj=Solution()
-print(obj.numofEnclaves(grid))
+print(obj.numberOfEnclaves(grid))
