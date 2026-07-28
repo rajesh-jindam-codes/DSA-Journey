42bbec348ebe98589944fd0c122e938fffcbc65e
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-28 10:06:17 +0530

    jump Game 2 leetcode 45

--- Arrays/JumpGame2.py
+++ Arrays/JumpGame2.py
@@ -1,25 +1,17 @@
-nums=[5,2,1,0,0,4,1,5]
-def jumpGame(index,jump,nums):
-<<<<<<< HEAD
-    if index>=len(nums)-1:
-        return jump
-    minJump=float('inf')
-    for i in range(1,nums[index]+1):
-        minJump=min(minJump,jumpGame(index+i,jump+1,nums))
-    return minJump
-print(jumpGame(0,0,nums))
-=======
-    n=len(nums)
-    jumps=0
-    left=0
-    right=0
-    while right<n-1:
-        farthest=0
-        for i in range(left,right+1):
-            farthest=max(farthest,i+nums[i])
-        left=right+1
-        right=farthest
-        jumps+=1
-    return jumps
-print(jumpGame(0,0,nums))
->>>>>>> 79cb04b (Jump Game 2 leetcode 45)
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        jumps = 0
+        left = 0
+        right = 0
+        while right < n - 1:
+            farthest = 0
+            for i in range(left, right + 1):
+                farthest = max(farthest, i + nums[i])
+            left = right + 1
+            right = farthest
+            jumps += 1
+        return jumps
+obj=Solution()
+nums=[2,3,1,1,4]
+print(obj.jump(nums))
