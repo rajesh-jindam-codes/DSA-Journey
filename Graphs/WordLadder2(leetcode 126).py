264ad0e0c58b68d5f5e544edc013f1246c30a11e
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-28 09:43:50 +0530

    find word ladders using BFS

--- Graphs/WordLadder2(leetcode 126).py
+++ Graphs/WordLadder2(leetcode 126).py
@@ -1,73 +1,44 @@
 from collections import defaultdict, deque
 class Solution:
-    def findLadders(self, beginWord, endWord, wordList):
-
-        wordSet = set(wordList)
-
+    def findLadders(self,beginWord,endWord,wordList):
+        wordSet=set(wordList)
         if endWord not in wordSet:
             return []
-
-        # Stores parent relationships
-        parents = defaultdict(list)
-
-        queue = deque([beginWord])
-
-        visited = set([beginWord])
-
-        found = False
-
+        parents=defaultdict(list)
+        queue=deque([beginWord])
+        visited=set([beginWord])
+        found=False
         while queue and not found:
-
-            local_visited = set()
-
+            localVisited=set()
             for _ in range(len(queue)):
-
-                word = queue.popleft()
-
-                word_chars = list(word)
-
-                for i in range(len(word_chars)):
-
-                    original = word_chars[i]
-
-                    for ch in "abcdefghijklmnopqrstuvwxyz":
-
-                        if ch == original:
+                word=queue.popleft()
+                wordChars=list(word)
+                for i in range(len(wordChars)):
+                    original=wordChars[i]
+                    for ch in 'abcdefghijklmnopqrstuvwxyz':
+                        if ch==original:
                             continue
-
-                        word_chars[i] = ch
-                        new_word = "".join(word_chars)
-
-                        if new_word in wordSet:
-
-                            # First time in this level
-                            if new_word not in visited:
-
-                                if new_word not in local_visited:
-                                    queue.append(new_word)
-                                    local_visited.add(new_word)
-
-                                parents[new_word].append(word)
-
-                            if new_word == endWord:
-                                found = True
-
-                    word_chars[i] = original
-
-            visited.update(local_visited)
-        result = []
-        # DFS to reconstruct paths
-        def dfs(word, path):
-            if word == beginWord:
+                        wordChars[i]=ch
+                        newWord="".join(wordChars)
+                        if newWord in wordSet:
+                            if newWord not in visited:
+                                if newWord not in localVisited:
+                                    queue.append(newWord)
+                                    localVisited.add(newWord)
+                                parents[newWord].append(word)
+                        if newWord==endWord:
+                            found =True
+                    wordChars[i]=original
+            visited.update(localVisited)
+        result=[]
+        def dfs(word,path):
+            if word==beginWord:
                 result.append(path[::-1])
                 return
-
             for parent in parents[word]:
-                dfs(parent, path + [parent])
-
+                dfs(parent,path+[parent])
         if found:
-            dfs(endWord, [endWord])
-
+            dfs(endWord,[endWord])
         return result
 beginWord='hit'
 endWord='cog'
