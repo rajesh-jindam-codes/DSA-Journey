eb0ada6a4bdb544adc006674010685e14feeb577
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-07-31 10:44:14 +0530

    fibonacci using recursion

---
+++ Dynamix Programming/Fibonacci/FibonacciusingRecursion.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fibonacci(self,n):
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        return self.fibonacci(n-1)+self.fibonacci(n-2)
+obj=Solution()
+print(obj.fibonacci(10))
