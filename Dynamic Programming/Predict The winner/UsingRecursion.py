e9882b8f9368ed0a0c48d6b048266583263b8a14
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-08-01 19:21:28 +0530

    predict winner using recursion leetcode 486

---
+++ Dynamic Programming/Predict The winner/UsingRecursion.py
@@ -0,0 +1,12 @@
+class Solution:
+    def solve(self,i,j,nums):
+        if i==j:
+            return nums[i]
+        pickLeft=nums[i]-self.solve(i+1,j,nums)
+        pickRight=nums[j]-self.solve(i,j-1,nums)
+        return max(pickLeft,pickRight)
+    def predictWinner(self,nums):
+        return self.solve(0,len(nums)-1,nums)>=0
+obj=Solution()
+nums=[1,5,2]
+print(obj.predictWinner(nums))
