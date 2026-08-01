744d28b15498cf6fcb1d18e11ff21e0cfc8e2110
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-08-01 12:11:50 +0530

    Topological Sort

--- Graphs/TopologicalSort.py
+++ Graphs/TopologicalSort.py
@@ -2,20 +2,19 @@ class Solution:
     def dfs(self,currNode,visited,stack,adjList):
         visited[currNode]=1
         for adjNode in adjList[currNode]:
-            if visited[adjNode]==0:
+            if visited[adjNode]==-1:
                 self.dfs(adjNode,visited,stack,adjList)
-            stack.append(currNode)
+        stack.append(currNode)
     def topoSort(self,V,edges):
         adjList=[[] for _ in range(V)]
-        visited=[0 for _ in range(V)]
+        visited=[-1 for _ in range(V)] 
         for u,v in edges:
             adjList[u].append(v)
+            # adjList[v].append(u)
         stack=[]
         for i in range(V):
-            if visited[i]==0:
+            if visited[i]==-1:
                 self.dfs(i,visited,stack,adjList)
         return stack[::-1]
 obj=Solution()
-V=6
-edges=[[5,2],[5,0],[4,0],[4,1],[2,3],[3,1]]
-print(obj.topoSort(V,edges))
+print(obj.topoSort(6,[[5,2],[5,0],[4,0],[4,1],[2,3],[3,1]]))
