afa4d08f35f2c7dca9ca99446fbcf74d11495832
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-08-03 10:46:21 +0530

    gray code leetcode 89

---
+++ Bit Manipulation/GrayCode.py
@@ -0,0 +1,9 @@
+class Solution:
+    def grayCode(self,n):
+        result=[]
+        for i in range(2**n):
+            gray=i^(i>>1)
+            result.append(gray)
+        return result
+obj=Solution()
+print(obj.grayCode(2))
