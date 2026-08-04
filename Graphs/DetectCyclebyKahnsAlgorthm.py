a15f034fcc3b40d20dd0b279e040a5752a300ecd
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-08-04 07:49:28 +0530

    Detect cycle in directed graph using kahns algorithm topological sort BFS

---
+++ Graphs/DetectCyclebyKahnsAlgorthm.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+# from Arrays.DegreeofArray import Solution
+def bfs(V,edges):
+    adjList=[[] for _ in range(V)]
+    indegrees=[0]*V
+    for u,v in edges:
+        adjList[u].append(v)
+        indegrees[v]+=1
+    result=[]
+    queue=deque()
+    for i in range(V):
+        if indegrees[i]==0:
+            queue.append(i)
+    while len(queue)!=0:
+        currNode=queue.popleft()
+        result.append(currNode)
+        for adjNode in adjList[currNode]:
+            indegrees[adjNode]-=1
+            if indegrees[adjNode]==0:
+                queue.append(adjNode)
+    if len(result)==V:
+        return False
+    return True
+V=3
+edges=[[0,1],[0,2],[1,2]]
+print(bfs(V,edges))
