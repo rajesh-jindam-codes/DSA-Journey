35f252d98fe476bcdb1d37adb04e1a65e3f67ce8
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-08-14 11:14:46 +0530

    median of two sorted arrays

--- Arrays/Medianof2SortedArrays(leetcode 4).py
+++ Arrays/Medianof2SortedArrays(leetcode 4).py
@@ -1,10 +1,10 @@
 nums1 = [1,2,3,4,6,9,8,7]
-nums2 = [9,8,7,6,5,4,8,3,0,12]
+nums2 = [9,8,7,6,5,4,8,3,0,12,15,15,19,8,3]
 nums3=nums1+nums2
 nums3.sort()
 n=len(nums3)
 
-if n%2==0:
+if n%2==1:
     median=nums3[n//2]
 else:
     median=(nums3[n//2-1]+nums3[n//2])/2
