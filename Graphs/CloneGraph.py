b2f5cd8aa770ac56f7145bde4fb6913a48c4056a
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-08-15 22:03:40 +0530

    Updated Clone Graph

--- Graphs/CloneGraph.py
+++ Graphs/CloneGraph.py
@@ -1,24 +1,54 @@
 from collections import deque
+
 class Node(object):
-    def __init__(self, val = 0, neighbors = None):
+    def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-class Solution:
-    def cloneGraph(self,node):
+
+class Solution(object):
+    def cloneGraph(self, node):
         if not node:
             return None
-        visited={}
-        visited[node]=Node(node.val)
-        queue=deque([node])
+
+        visited = {}
+
+        # Create clone of starting node
+        visited[node] = Node(node.val)
+
+        queue = deque([node])
+
         while queue:
-            curr=queue.popleft()
-            for nei in curr.neighbours:
+            curr = queue.popleft()
+
+            for nei in curr.neighbors:
+
+                # If neighbor hasn't been cloned
                 if nei not in visited:
-                    visited[nei]=Node(nei.val)
+                    visited[nei] = Node(nei.val)
                     queue.append(nei)
-                visited[curr].neighbours.append(visited[nei])
+
+                # Connect cloned current node to cloned neighbor
+                visited[curr].neighbors.append(visited[nei])
+
         return visited[node]
 adjList = [[2,4],[1,3],[2,4],[1,3]]
-obj=Solution()
-print(obj.cloneGraph(1))
+# Create the original graph
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node1.neighbors = [node2, node4]
+node2.neighbors = [node1, node3]
+node3.neighbors = [node2, node4]
+node4.neighbors = [node1, node3]
+
+
+# Clone graph
+obj = Solution()
+clone = obj.cloneGraph(node1)
+
+# Check result
+print(clone.val)
+print([neighbor.val for neighbor in clone.neighbors])
