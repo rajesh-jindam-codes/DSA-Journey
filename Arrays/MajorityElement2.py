401d32e3053f70231b4f70ce7c123c8631881bbc
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-03 18:58:50 +0530

    Majority Element 2

--- Arrays/MajorityElement2.py
+++ Arrays/MajorityElement2.py
@@ -3,7 +3,7 @@ def majorityElement(nums):
     count1=count2=0
     for num in nums:
         if candy1==num:
-            count+=1
+            count1+=1
         elif candy2==num:
             count2+=1
         elif count1==0:
@@ -13,13 +13,14 @@ def majorityElement(nums):
             candy2=num
             count2=1
         else:
-            count-=1
+            count1-=1
             count2-=1
+
     result=[]
     if nums.count(candy1)>len(nums)//3:
         result.append(candy1)
     if candy2!=candy1 and nums.count(candy2)>len(nums)//3:
         result.append(candy2)
     return result
-nums=[1,2,3,1,1,2,2]
+nums=[1,1,1,3,3,2,2,2]
 print(majorityElement(nums))
