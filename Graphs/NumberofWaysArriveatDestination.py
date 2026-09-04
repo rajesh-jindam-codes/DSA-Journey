d9ce03ba19dab290061ee2638648677c934a8f49
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-04 10:28:00 +0530

    Number of ways to arrive at destination leetcode 1976

--- Graphs/NumberofWaysArriveatDestination.py
+++ Graphs/NumberofWaysArriveatDestination.py
@@ -1,14 +1,16 @@
 import heapq
+n=7
+roads=[[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
 def countPaths(n,roads):
-    MOD=10**9+7
     adjList=[[] for _ in range(n)]
+    MOD=10**9+7
     for u,v,wt in roads:
         adjList[u].append([v,wt])
         adjList[v].append([u,wt])
     distance=[float('inf') for _ in range(n)]
     ways=[0 for _ in range(n)]
-    ways[0]=1
     distance[0]=0
+    ways[0]=1
     queue=[[0,0]]
     while queue:
         dist,node=heapq.heappop(queue)
@@ -22,7 +24,6 @@ def countPaths(n,roads):
                 ways[adjNode]=ways[node]
             elif newDist==distance[adjNode]:
                 ways[adjNode]+=ways[node]
-    return ways[n-1]%MOD
-n=7
-roads=[[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
+    return ways[n-1] % MOD
+
 print(countPaths(n,roads))
