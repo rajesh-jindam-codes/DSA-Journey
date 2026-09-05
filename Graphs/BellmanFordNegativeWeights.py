152392078d92125fb4134e72846c5229f86f4f2c
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-05 15:35:19 +0530

    Bellman ford algorithm to find negative cycles

--- Graphs/BellmanFordNegativeWeights.py
+++ Graphs/BellmanFordNegativeWeights.py
@@ -1,16 +1,15 @@
 def bellmanFord(V,edges,src):
-    INF=10**8
-    dist=[INF for _ in range(V)]
+    inf=10**8
+    dist=[inf for _ in range(V)]
     dist[src]=0
-    for _ in range(V-1):
+    for i in range(V-1):
         for u,v,w in edges:
-            if dist[u]+w <dist[v] and dist[u]!=INF:
+            if dist[u]!=inf and dist[u]+w<dist[v]:
                 dist[v]=dist[u]+w
     for u,v,w in edges:
-        if dist[u]+w<dist[v] and dist[u]!=INF:
-            return -1
+        if dist[u]!=inf and dist[u]+w<dist[v]:
+            dist[v]=dist[u]+w
     return dist
 V=5
 edges=[[1, 3, 2], [4, 3, -1], [2, 4, 1], [1, 2, 1], [0, 1, 5]]
-src=0
-print(bellmanFord(V,edges,src))
+print(bellmanFord(V,edges,0))
