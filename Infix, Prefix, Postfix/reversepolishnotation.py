31b7a0ec56f4abf119251cc8d5897d3d992843a8
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-06 11:05:11 +0530

    Evaluate reverse polish notation

---
+++ Infix, Prefix, Postfix/reversepolishnotation.py
@@ -0,0 +1,18 @@
+def evaluateRPN(tokens):
+    stack=[]
+    for token in tokens:
+        if token not in "+-*/":
+            stack.append(int(token))
+        else:
+            b=stack.pop()
+            a=stack.pop()
+            if token=='*':
+                stack.append(a*b)
+            elif token=='+':
+                stack.append(a+b)
+            elif token=='/':
+                stack.append(a-b)
+            else:stack.append(int(a/b))
+    return stack[-1]
+tokens=["2","1","+","3","*"]
+print(evaluateRPN(tokens))
