063beb695506cbd25e790f462c4c2e6d291a3697
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-07 15:05:04 +0530

    Minimum path sum leetcode 64

---
+++ Graphs/minPathSum.py
@@ -0,0 +1,14 @@
+def minPath(grid):
+    rows=len(grid)
+    cols=len(grid[0])
+    def solve(i,j):
+        if i==0 and j==0:
+            return grid[0][0]
+        if i<0 or j<0:
+            return float('inf')
+        up=solve(i-1,j)
+        left=solve(i,j-1)
+        return grid[i][j]+min(up,left)
+    return solve(rows-1,cols-1)
+grid=[[1,3,1],[1,5,1],[4,2,1]]
+print(minPath(grid))
--- Infix, Prefix, Postfix/reversepolishnotation.py
+++ Infix, Prefix, Postfix/reversepolishnotation.py
@@ -1,18 +1,24 @@
-def evaluateRPN(tokens):
-    stack=[]
-    for token in tokens:
-        if token not in "+-*/":
-            stack.append(int(token))
-        else:
-            b=stack.pop()
-            a=stack.pop()
-            if token=='*':
-                stack.append(a*b)
-            elif token=='+':
-                stack.append(a+b)
-            elif token=='/':
-                stack.append(a-b)
-            else:stack.append(int(a/b))
-    return stack[-1]
-tokens=["2","1","+","3","*"]
-print(evaluateRPN(tokens))
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack=[]
+        for token in tokens:
+            if token not in '+-*/':
+                stack.append(int(token))
+            else:
+                b=stack.pop()
+                a=stack.pop()
+                if token=='+':
+                    stack.append(a+b)
+                elif token =='-':
+                    stack.append(a-b)
+                elif token=='*':
+                    stack.append(a*b)
+                else:
+                    stack.append(int(a/b))
+        return stack[-1]
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+print(Solution().evalRPN(tokens))
