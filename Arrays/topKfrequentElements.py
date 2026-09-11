93db906aa45cb09a6333c2977f1d9508ae3a7d3a
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-11 20:00:28 +0530

    Top k frequent elements leetcode 347

--- Arrays/topKfrequentElements.py
+++ Arrays/topKfrequentElements.py
@@ -1,9 +1,9 @@
 from collections import Counter
 from typing import List
-nums=[1,2,1,2,2,1,2,3,3,3,4,4,4,4]
-k=3
-bucket=[[] for _ in range(len(nums)+1)]
+nums=[1,2,3,2,3,1,4]
 freq=Counter(nums)
+k=2
+bucket=[[] for _ in range(len(nums)+1)]
 for num,count in freq.items():
     bucket[count].append(num)
 result=[]
