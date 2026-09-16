34041e003f733b4368e17782ccb2f7f87d9b3eeb
Rajesh Jindam <rajeshjindam8@gmail.com>
2026-09-16 16:28:50 +0530

    Minimum value in binary search tree

---
+++ BinarySearchTree/minimumValue inBST.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+        current = self.root
+
+        while True:
+            if value < current.value:
+                if current.left is None:
+                    current.left = Node(value)
+                    return
+                current = current.left
+
+            elif value > current.value:
+                if current.right is None:
+                    current.right = Node(value)
+                    return
+                current = current.right
+
+            else:
+                # Duplicate values are ignored
+                return
+    def inorder(self, node):
+        if node is None:
+            return
+
+        self.inorder(node.left)
+        print(node.value, end=" ")
+        self.inorder(node.right)
+
+    def minimumValue(self,root):
+        while root is not None and root.left is not None:
+            root=root.left
+        return root.value
+# Create BST
+tree = BST()
+
+# Insert values
+values = [50, 30, 70, 20, 40, 60, 80,1]
+
+for value in values:
+    tree.insert(value)
+
+# Inorder traversal
+tree.inorder(tree.root)
+
+# Search
+# print("\nFound:", tree.search(40))
+# print("Found:", tree.search(100))
+print("\n",tree.minimumValue(tree.root))
